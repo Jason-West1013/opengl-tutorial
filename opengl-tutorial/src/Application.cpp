308e0c060380cd29ee5a27bf91ecb452b527c1f1
Jason West <jasonwest1013@yahoo.com>
2023-04-03 21:23:50 -0400

    add vertex array object

--- opengl-tutorial/src/Application.cpp
+++ opengl-tutorial/src/Application.cpp
@@ -111,6 +111,10 @@ int main(void)
     if (!glfwInit())
         return -1;
 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
     if (!window)
@@ -141,6 +145,11 @@ int main(void)
         2, 3, 0
     };
 
+    // Vertex Array 
+    unsigned int vertexArrayObject;
+    glGenVertexArrays(1, &vertexArrayObject);
+    glBindVertexArray(vertexArrayObject);
+
     // Vertex Buffer
     unsigned int buffer;
     glGenBuffers(1, &buffer);
@@ -166,6 +175,11 @@ int main(void)
     ASSERT(location != -1);
     glUniform4f(location, 0.2f, 0.3f, 0.8f, 1.0f);
 
+    glBindVertexArray(0);
+    glUseProgram(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
     float r = 0.0f;
     float increment = 0.05f;
 
@@ -175,7 +189,13 @@ int main(void)
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
+        glUseProgram(shader);
         glUniform4f(location, r, 0.3f, 0.8f, 1.0f);
+
+        glBindVertexArray(vertexArrayObject);
+
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBufferObject);
+        
         GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
 
         if (r > 1.0f)
